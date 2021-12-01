10b9621ae9a7d2583ab004e2a39f69fb714f64cb
mujahed85 <mujahed.mca@gmail.com>
2021-12-01 16:57:49 +0530

    Delete an Empty Bucket

---
+++ 06.S3BucketDelete.py
@@ -0,0 +1,14 @@
+##client --> delete_bucket(), resource --> bucket.delete()
+
+import boto3
+
+# s3client=boto3.client("s3")
+# s3client.delete_bucket(Bucket="bkt0337mujahed")
+# print("Bucket Deleted")
+
+# s3resource=boto3.resource("s3")
+# bucketName=input("Enter Bucket Name to Delete: ")
+# Bucket=s3resource.Bucket(bucketName)
+# Bucket.delete()
+# print("Bucket Deleted")
+
